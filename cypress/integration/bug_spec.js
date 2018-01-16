2e08982619b5fdaa0ffede49ddc176124b1b7b18
Zak Henry <zak@cloudnc.com>
2018-01-16 11:50:40 +0000

    Add tests to verify behavior

--- cypress/integration/bug_spec.js
+++ cypress/integration/bug_spec.js
@@ -1,8 +1,12 @@
 describe('Test bug', () => {
 
-  it('should error', () => {
+  it('should not have errors', () => {
 
     cy.visit('http://localhost:8080');
+
+    cy.wait(100000);
+
+    cy.get('#errors').should('contain', 'No Errors');
 
   });
 
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,54 @@
 import { ArcRotateCamera, DirectionalLight, Engine, Mesh, Scene, ShadowGenerator, Vector3 } from 'babylonjs';
 
-
 try {
 
-  const canvas: HTMLCanvasElement = document.getElementById('babylon') as HTMLCanvasElement;
+  const progress  = document.getElementById('progress');
 
+  progress.innerHTML += `loading canvas\n`;
+  const canvas: HTMLCanvasElement = document.getElementById('babylon') as HTMLCanvasElement;
+  progress.innerHTML += `starting engine\n`;
   const engine = new Engine(canvas);
 
+  progress.innerHTML += `creating scene\n`;
   const scene = new Scene(engine);
 
+  progress.innerHTML += `registering camera\n`;
   const camera = new ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 50, Vector3.Zero(), scene);
 
+  progress.innerHTML += `attaching camera to canvas\n`;
   camera.attachControl(canvas, true);
 
+  progress.innerHTML += `creating light\n`;
   const light = new DirectionalLight("direct", new Vector3(-1.2, -1, -2).normalize(), scene);
   light.position = new Vector3(10, 30, 15);
 
   // Creation of a knot
   // (name, radius, tube, radialSegments, tubularSegments, p, q, scene, updatable)
+  progress.innerHTML += `creating knot\n`;
   const knot = Mesh.CreateTorusKnot("knot", 5, 0.5, 128, 64, 2, 3, scene);
 
+  progress.innerHTML += `creating ground\n`;
   const ground = Mesh.CreateGround("ground", 30,30, 10, scene);
   ground.position.y = -8;
 
+  progress.innerHTML += `creating shadows\n`;
   const shadowGenerator = new ShadowGenerator(4096, light);
   shadowGenerator.getShadowMap().renderList.push(knot);
   knot.receiveShadows = true;
   ground.receiveShadows = true;
 
+  progress.innerHTML += `starting render loop\n`;
   engine.runRenderLoop(() => {
     scene.render();
   });
 
+  progress.innerHTML += `setting 'No Errors'\n`;
+  document.getElementById('errors').innerHTML = 'No Errors';
 
 } catch (e) {
 
-  document.write('Encountered error: ' + e.message)
+  console.error(e);
+  document.getElementById('errors').innerHTML = 'Encountered error: ' + e.message;
+
 
 }
